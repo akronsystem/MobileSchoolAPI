20a7c7b6320ee900e2a5d962db6aceddb5e08c72
akronsystem <support@akronsystems.com>
2018-10-20 20:57:54 +0530

    merging changes

--- MobileSchoolAPI/Controllers/DashboardController.cs
+++ MobileSchoolAPI/Controllers/DashboardController.cs
@@ -1,6 +1,4 @@
-﻿using MobileSchoolAPI.BusinessLayer;
-using MobileSchoolAPI.Models;
-using MobileSchoolAPI.ParamModel;
+﻿using MobileSchoolAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,24 +10,16 @@ namespace MobileSchoolAPI.Controllers
 {
     public class DashboardController : ApiController
     {
-		
-		[HttpGet]
-		public object GetStudentInfo([FromBody] ParamDashboardStudentInfo StudentInfo)
-		{
-			try
-			{
-				DashboardManager objmanager = new DashboardManager();
-				var student = objmanager.GetStudentInfo(StudentInfo);
-				if (student == null)
-					return new Error() { IsError = true, Message = "Studnet Info Not Found" };
+		SchoolContext db = new SchoolContext();
 
-				return student;
-			}
-			catch(Exception ex)
-			{
-				return new Error() { IsError = true, Message = ex.Message};
-			}
-			
+		[HttpGet]
+		public object GetStudentInfo(int StudentId,string token)
+		{
+			var student = db.VWSTUDENTINFO.FirstOrDefault(r => r.STUDENTID == StudentId);
+			if(student==null)
+				return new Error() { IsError = true, Message = "Studnet Info Not Found" };
+
+			return student;
 
 		}
 
