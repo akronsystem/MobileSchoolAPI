eefbd7d6474ac82d613641ad5039bfc4f0e9256a
abhishek hawal <44230360+abhishekhawal1578@users.noreply.github.com>
2018-10-24 17:18:55 +0530

    Update MobileSchoolAPI/Models/GETSTUDENTPARAM.cs
    CREATE GETSTUDENTPARAM MODEL FOR DECLARING PROPERTIES

---
+++ MobileSchoolAPI/Models/GETSTUDENTPARAM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileSchoolAPI.Models
+{
+    public class GETSTUDENTPARAM
+    {
+
+        public DateTime DATE { get; set; }
+        public string STANDARDID { get; set; }
+        public int DIVISIONID { get; set; }
+
+    }
+}
