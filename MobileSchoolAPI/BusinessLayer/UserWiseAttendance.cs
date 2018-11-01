e15d7ce6d190d0d36e71a1d60e7e13bd376bd1b2
ankitagalchat <44231271+ankitagalchat@users.noreply.github.com>
2018-11-01 17:56:46 +0530

    Modified THe Result Returned

--- MobileSchoolAPI/BusinessLayer/UserWiseAttendance.cs
+++ MobileSchoolAPI/BusinessLayer/UserWiseAttendance.cs
@@ -13,22 +13,26 @@ namespace MobileSchoolAPI.BusinessLayer
 
         public object GetAttendanceByUser(ParamDateWiseAttendance obj)
         {
+            Result objRES = new Result();
             try
             {
-                var usertype= objSc.VW_GET_USER_TYPE.Where(r => r.UserId == obj.UserId).ToList();
+                var usertype = objSc.VW_GET_USER_TYPE.Where(r => r.UserId == obj.UserId).ToList();
 
                 if (usertype[0].UserType == "STUDENT")
                 {
                     var StudentAttendance = objSc.VWATTENDANCEBYDATESTUDENTs.Where(r => r.UserId == obj.UserId && r.DIVISIONID == obj.DivisionId && r.ATTEDANCEDATE == obj.AttendanceDate).ToList();
                     if (StudentAttendance.Count() == 0)
                     {
-                        return "Status : Present";
-                        //EMPLOYEE logic
-                        // return new Error() { IsError = true, Message = "Attendance not found" };
+                        objRES.Date = Convert.ToDateTime(obj.AttendanceDate);
+                        objRES.Status = "Present";
+                        return objRES;
                     }
+                    
                     else
                     {
-                        return "Status : Absent";
+                        objRES.Date = Convert.ToDateTime(StudentAttendance[0].ATTEDANCEDATE);
+                        objRES.Status = "Absent";
+                        return objRES;
                     }
                 }
                 else
@@ -36,22 +40,36 @@ namespace MobileSchoolAPI.BusinessLayer
                     var EMPATTENDANCE = objSc.VWATTENDANCEEMPLOYEEs.Where(r => r.UserId == obj.UserId && r.ATTEDANCEDATE == obj.AttendanceDate && r.DISPLAY == 1).ToList();
                     if (EMPATTENDANCE.Count() == 0)
                     {
-                        return "User Is Not Class Teacher";
+                        objRES.Date = Convert.ToDateTime(obj.AttendanceDate);
+                        objRES.Status = "User Is Not Class Teacher";
+                        return objRES;
+                       
 
                     }
                     else
                     {
-                        return "Status : Attendance Completed;";
+                        objRES.Date = Convert.ToDateTime(EMPATTENDANCE[0].ATTEDANCEDATE);
+                        objRES.Status = "Attendance Completed;";
+                        return objRES;
+
+                       
                     }
                 }
-                           
-                
+
+
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 return new Error() { IsError = true, Message = e.Message };
 
             }
         }
+
+            public class Result
+            {
+            public DateTime Date { get; set; }
+               public string Status { get; set; }
+    }
+        
     }
 }
