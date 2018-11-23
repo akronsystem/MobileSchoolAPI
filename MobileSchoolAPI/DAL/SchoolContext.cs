b4e3a83fceb71a23cf0edc1ce629c964565dcdb5
abhishek hawal <44230360+abhishekhawal1578@users.noreply.github.com>
2018-11-23 17:06:07 +0530

    Update SchoolContext.cs
    CREATE NEW VIEW FOR NOTIFICATION

--- MobileSchoolAPI/DAL/SchoolContext.cs
+++ MobileSchoolAPI/DAL/SchoolContext.cs
@@ -68,8 +68,8 @@ namespace MobileSchoolAPI.Models
         public virtual DbSet<VIEWDIVISIONLISTBYSTUDENT> VIEWDIVISIONLISTBYSTUDENTs { get; set; }
         public virtual DbSet<VIEWDIVISIONWISESUBJECTSTUDENT> VIEWDIVISIONWISESUBJECTSTUDENTs { get; set; }
         public virtual DbSet<Vw_ATTENDANCECHECK> Vw_ATTENDANCECHECK { get; set; }
-      
 
+        public virtual DbSet<VIEWHOLIDAY> VIEWHOLIDAYs { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
 		{
 			modelBuilder.Entity<TBLUSERLOGIN>()
