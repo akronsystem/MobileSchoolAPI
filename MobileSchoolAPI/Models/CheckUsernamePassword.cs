1aea28e95652a59327833e10dd321419aadc631b
nita chiparge <nitachiparge112@gmail.com>
2018-11-27 16:00:26 +0530

    Update CheckUsernamePassword.cs

--- MobileSchoolAPI/Models/CheckUsernamePassword.cs
+++ MobileSchoolAPI/Models/CheckUsernamePassword.cs
@@ -7,10 +7,11 @@ namespace MobileSchoolAPI.Models
 {
     public class CheckUsernamePassword
     {
-        SchoolContext objSC = new SchoolContext();
+       
         public bool ValidateUsernamePassword(Int32 USERID,string PASSWORD)
         {
-           // string pwd = CryptIt.Encrypt(PASSWORD);
+            SchoolMainContext objSC = new ConcreateContext().GetContext(USERID, PASSWORD);
+            // string pwd = CryptIt.Encrypt(PASSWORD);
             var usernamepwd = objSC.VW_GET_USER_TYPE.Where(r => r.UserId == USERID && r.Password == PASSWORD).ToList();
             if(usernamepwd.Count>0)
             {
