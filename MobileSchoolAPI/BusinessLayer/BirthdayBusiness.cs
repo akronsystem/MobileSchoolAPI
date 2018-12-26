3e3aa30f015d687e9684aaba46cf231bd5ba7f56
Viveks775 <vivek.shinde@akronsystems.com>
2018-12-26 17:51:07 +0530

    Controller - Dashboard - Get Today's Birthday List , Get Institute Name
    Controller - Dashboard - Get Today's Birthday List , Get Institute Name

---
+++ MobileSchoolAPI/BusinessLayer/BirthdayBusiness.cs
@@ -0,0 +1,36 @@
+﻿using MobileSchoolAPI.Models;
+using MobileSchoolAPI.ResultModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileSchoolAPI.BusinessLayer
+{
+    public class BirthdayBusiness
+    {
+        public object GetTodayBirthDay(GetUserId Obj)
+        {
+            SchoolMainContext db = new ConcreateContext().GetContext(Obj.UserId, Obj.PASSWORD);
+            if (db == null)
+            {
+                return new ResultBirth { IsSuccess = false, Result = "User Id or Password is Incorrect" };
+            }
+            else
+            {
+                //to get todays birthday list ViewGetTodayBirthDetail
+
+                var result = db.ViewGetTodayBirthDetails.ToList();
+                if (result == null)
+                {
+                    return new ResultBirth { IsSuccess = false, Result = "No Birthdays Found Today" };
+                }
+                else
+                {
+                    return new ResultBirth { IsSuccess = true, Result = result };
+                }
+            }
+
+        }
+    }
+}
