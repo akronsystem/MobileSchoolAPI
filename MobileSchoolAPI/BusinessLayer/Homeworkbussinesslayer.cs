c8b406050e7a11ac6036160385cfb242ca15e6e4
nishigandhabidkar <44231294+nishigandhabidkar@users.noreply.github.com>
2019-02-20 17:11:19 +0530

    Chnages  related to attendance Save
    change in homework business file of sending attendance notification

--- MobileSchoolAPI/BusinessLayer/Homeworkbussinesslayer.cs
+++ MobileSchoolAPI/BusinessLayer/Homeworkbussinesslayer.cs
@@ -232,6 +232,7 @@ namespace MobileSchoolAPI.BusinessLayer
 
                         if (smsstatus == "1")
                         {
+
                             string responseString =  SMSSendTESTDLR(getstudent[0].GMOBILE, txtMessage, logindetail.UserName);
                             if(responseString!="")
                             {
