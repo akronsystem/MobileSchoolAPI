10c9f450b1d0730ecd1bd00c9c77d37cb8d6e077
akronsystem <support@akronsystems.com>
2019-03-02 00:04:01 +0530

    changes 1-03-2018

--- MobileSchoolAPI/App_Start/AddDefaultResponse.cs
+++ MobileSchoolAPI/App_Start/AddDefaultResponse.cs
@@ -11,7 +11,7 @@ namespace AgroApp.App_Start
     {
         public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
         {
-            if (operation.operationId == "HomeWork_TestUpload")
+            if (operation.operationId == "HomeWork_HomeworkSave")
             {
                 operation.consumes.Add("multipart/form-data");
                 operation.parameters = new[]
